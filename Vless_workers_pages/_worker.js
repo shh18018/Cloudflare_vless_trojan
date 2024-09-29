6fa754c6881c8e3d6ac0bb3d6dc3490ab77dd72a
shh18018 <hao154980362@gmail.com>
2024-09-29 20:25:43 +0800

    Update _worker.js

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "77a571fb-4fd2-4b37-8596-1b7d9728bb5c";
+let userID = "19b1f42c-e8d5-419d-be04-667523a77877";
 
 const proxyIPs = ["proxy.xxxxxxxx.tk"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 const cn_hostnames = [''];
